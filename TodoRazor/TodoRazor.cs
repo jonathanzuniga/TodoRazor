512ba140ad83785b20772c380e7a0e6a3b1c2c8d
Jonathan Zúñiga <jonathanzuniga@gmail.com>
2016-04-20 11:13:41 -0700

    Initial commit.

---
+++ TodoRazor/TodoRazor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace TodoRazor
+{
+	public class App : Application
+	{
+		public App ()
+		{
+			var template = new Login ();
+			var page = template.GenerateString ();
+			var html = new HtmlWebViewSource { Html = page };
+
+			// The root page of your application.
+			MainPage = new ContentPage {
+				Content = new WebView () {
+					Source = html
+				}
+			};
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
